70b7190d318f964062935ad719d986b47c68ec60
Tobi <tobias@wylega.de>
2023-12-01 17:47:51 +0100

    add buttons for all days

--- src/components/Keyboard/index.tsx
+++ src/components/Keyboard/index.tsx
@@ -16,8 +16,29 @@ export const Keyboard = () => {
 	return (
 		<VStack spacing={2}>
 			<HStack spacing={2}>
-				{[1, 2, 3, 4, 5].map((n) => (
-					<KeyboardButton key={n} onClick={() => onNumberClick(n)}>
+				{[2, 3, 4, 5, 6].map((n) => (
+					<KeyboardButton key={n} onClick={() => onNumberClick(n-1)}>
+						{n}
+					</KeyboardButton>
+				))}
+			</HStack>
+			<HStack spacing={2}>
+				{[8, 9, 10, 11, 12].map((n) => (
+					<KeyboardButton key={n} onClick={() => onNumberClick(n-7)}>
+						{n}
+					</KeyboardButton>
+				))}
+			</HStack>
+			<HStack spacing={2}>
+				{[14, 15, 16, 17, 18].map((n) => (
+					<KeyboardButton key={n} onClick={() => onNumberClick(n-13)}>
+						{n}
+					</KeyboardButton>
+				))}
+			</HStack>
+			<HStack spacing={2}>
+				{[20, 21, 22, 23, 24].map((n) => (
+					<KeyboardButton key={n} onClick={() => onNumberClick(n-19)}>
 						{n}
 					</KeyboardButton>
 				))}
